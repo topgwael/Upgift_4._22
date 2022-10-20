9dbb45bda859facdc5e8486e54959b32ccb577f0
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-10-20 13:31:06 +0200

    Add project files.

---
+++ Upgift_4._22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Upgift_4._22
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 180; i < 201; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1000; i < 1401; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
